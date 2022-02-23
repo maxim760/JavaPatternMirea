d4774415cd096b03f30ff5e35c4b7b1935a1a7dd
Maksim <maks.tomanov@mail.ru>
2022-02-23 23:01:03 +0300

    task11 jar file

--- src/com/company/task11/src/main/java/com/example/task11/Task11Application.java
+++ src/com/company/task11/src/main/java/com/example/task11/Task11Application.java
@@ -3,6 +3,15 @@ package com.example.task11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Войдите в режим командной строки (команда cmd), перейдите в папку с pom.xml и введите команду mvn clean package
+  Maven cгенерирует исполняемый jar-файл с именем SpringBootRestService-1.0.jar
+  Перейдите в папку cd target
+  Затем запустите jar-файл: java -jar task11-0.0.1-SNAPSHOT.jar
+  Перейдите в браузере по адресу http://localhost:8080/
+*
+* */
+
 // ./mvnw spring-boot:run
 
 @SpringBootApplication
