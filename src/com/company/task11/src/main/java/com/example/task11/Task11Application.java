a009ab96d84812b8d3e644bf500318f5c085a10d
Maksim <maks.tomanov@mail.ru>
2022-02-23 22:46:22 +0300

    task11

---
+++ src/com/company/task11/src/main/java/com/example/task11/Task11Application.java
@@ -0,0 +1,15 @@
+package com.example.task11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// ./mvnw spring-boot:run
+
+@SpringBootApplication
+public class Task11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task11Application.class, args);
+	}
+
+}
