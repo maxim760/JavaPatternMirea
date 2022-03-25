2056dbbbe6e9ff86186a6cc6a26884f250951c97
Maksim <maks.tomanov@mail.ru>
2022-03-25 14:29:00 +0300

    remove package

--- src/com/company/task23/src/main/java/com/example/task21/Response/ErrorResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.task21.Response;
-
-public class ErrorResponse extends UserResponse {
-    public ErrorResponse(String message) {
-        super(null, message, false);
-    }
-}
--- src/com/company/task23/src/main/java/com/example/task21/repository/DogRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.task21.repository;
-
-import com.example.task21.entity.DogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DogRepo extends JpaRepository<DogEntity, Long> {
-}
