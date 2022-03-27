2d5b3581239654a2460a840db939a03dc67776e6
Maksim <maks.tomanov@mail.ru>
2022-03-28 00:39:45 +0300

    finish task 9

--- src/com/company/task9/Main.java
+++
@@ -1,7 +0,0 @@
-package src.com.company.task9;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
---
+++ src/com/company/task9/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+// ./gradlew build
+// ./gradlew hello
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Docker runned");
+	}
+
+}
